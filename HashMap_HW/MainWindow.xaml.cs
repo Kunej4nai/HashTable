bfac8e86dc4b66ba8ae4835f2edd30011bc58660
kune <110716320+Kunej4nai@users.noreply.github.com>
2022-08-07 16:55:43 +0700

    Add files via upload

---
+++ HashMap_HW/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HashMap_HW
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+{ public partial class MainWindow : Window
+
+  {
+    Hashtable hashtable;
+    public MainWindow()
+    { 
+        InitializeComponent();
+        hashtable = new Hashtable();
+     }
+
+    private void Add_Botton_Click(object sender, RoutedEventArgs e)
+    {   //add
+        hashtable.Add(Key_txtbox.Text, Value_txtbox.Text);
+
+        Key_txtbox.Text = String.Empty;
+        Value_txtbox.Text = string.Empty;
+
+    }
+   
+        
+        private void ReMove_Botton_Click(object sender, RoutedEventArgs e)
+     {   //remove
+        hashtable.Remove(Key_txtbox.Text);
+       
+        Key_txtbox.Text = String.Empty;     
+        Value_txtbox.Text = String.Empty;  
+      }
+        
+
+        private void ShowAll_Button_Click(object sender, RoutedEventArgs e)
+        {   //show
+            ICollection iconllection = hashtable.Keys;
+            foreach (string key in iconllection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+    }
+}
